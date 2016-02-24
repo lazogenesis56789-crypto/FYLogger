201f45b32b92e347bd5dabb9c00586513fc2a16e
syxc <syxc@taohaikeji.com>
2016-02-24 14:04:12 +0800

    Modified

---
+++ Package.swift
@@ -0,0 +1,13 @@
+//
+//  Package.swift
+//  FYLogger
+//
+//  Created by syxc on 16/2/23.
+//  Copyright © 2016年 syxc. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+  name: "FYLogger"
+)
